2f818f6147c5734040e36b42cc879d36683313dc
Ivipop <157631466+Ivipop@users.noreply.github.com>
2024-01-24 19:35:09 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,12 +6,6 @@ urlpatterns = [
     path('<str:lang>/', views.IndexGet ),
     path('r', views.RechercheGet ),
     path('<str:lang>/r', views.RechercheGet ),
-    path('c/c', views.AjoutView.as_view() ),
-    path('<str:lang>/c/c', views.AjoutView.as_view() ),
-    path('c/o', views.AjoutOptionView.as_view() ),
-    path('<str:lang>/c/o', views.AjoutOptionView.as_view() ),
-    path('c/i', views.AjoutReponseGet ),
-    path('<str:lang>/c/i', views.AjoutReponseGet ),
     path('<str:slug>', views.MentionsGet ),
     path('<str:lang>/<str:slug>', views.MentionsGet ),
 ]
